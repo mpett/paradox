739b8770bc57dde5567823225449aaa43d574074
Martin Pettersson <kontakt@martinpettersson.se>
2017-11-13 12:38:04 +0100

    Refactored.

--- VectorDistance.java
+++ VectorDistance.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.*;
 
 import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
 public class VectorDistance {
@@ -23,11 +24,12 @@ public class VectorDistance {
         long elapsedTime = 0L;
         mId = materialIndices(MATERIAL_IDS);
         materials = parseMaterials(mId);
-
         Set<Integer> keys = materials.keySet();
-        System.err.println("Number of materials in hash map: " + keys.size());
+        System.err.println
+                ("Number of materials in hash map: " + keys.size());
         elapsedTime = (new Date()).getTime() - startTime;
         System.err.println("Time after parse: " + elapsedTime);
+
         unsortEnergy();
         elapsedTime = (new Date()).getTime() - startTime;
         System.err.println("Time after unsorting: " + elapsedTime);
@@ -36,8 +38,12 @@ public class VectorDistance {
             Material m = materials.get(id);
             double[] y = m.toPrimitive(m.getEnergy());
             double[] x = m.toPrimitive(m.getDos());
+            SplineInterpolator si = new SplineInterpolator();
             LinearInterpolator li = new LinearInterpolator();
             PolynomialSplineFunction psf = li.interpolate(x, y);
+            PolynomialSplineFunction psf2 = si.interpolate(x, y);
+            m.setPsf(psf);
+            m.setPsf2(psf2);
         }
         
         elapsedTime = (new Date()).getTime() - startTime;
@@ -89,7 +95,8 @@ public class VectorDistance {
                 indicesToBeRemoved.add(materialId);
                 continue;
             }
-            Material material = new Material(energyVector, dosVector, materialId);
+            Material material =
+                    new Material(energyVector, dosVector, materialId);
 
             materials.put(materialId, material);
         }
@@ -129,20 +136,25 @@ public class VectorDistance {
     private static void unsortEnergy() {
         for (int materialId : mId) {
             Material m = materials.get(materialId);
-            HashMap<Double, Double> dosEnergyMap = new HashMap<>();
+            HashMap<Double, Double> dosEnergyMap =
+                    new HashMap<>();
             ArrayList<Double> dos = m.getDos();
             ArrayList<Double> energy = m.getEnergy();
             for (int index = 0; index < dos.size(); index++) {
-                dosEnergyMap.put(dos.get(index), energy.get(index));
+                dosEnergyMap.put(
+                        dos.get(index),
+                            energy.get(index));
             }
             Set<Double> dosSet = new HashSet<>();
             dosSet.addAll(dos);
             dos.clear();
             dos.addAll(dosSet);
             Collections.sort(dos);
-            ArrayList<Double> unsortedEngergy = new ArrayList<>();
+            ArrayList<Double> unsortedEngergy =
+                    new ArrayList<>();
             for (double dosValue : dos) {
-                unsortedEngergy.add(dosEnergyMap.get(dosValue));
+                unsortedEngergy
+                        .add(dosEnergyMap.get(dosValue));
             }
             m.setEnergy(unsortedEngergy);
             m.setDos(dos);
@@ -169,6 +181,8 @@ class Material {
     private ArrayList<Double> dos;
     private int materialId;
     private final double[] EMPTY_DOUBLE_ARRAY = {};
+    private PolynomialSplineFunction psf;
+    private PolynomialSplineFunction psf2;
 
     public Material(ArrayList<Double> energy,
                     ArrayList<Double> dos,
@@ -176,6 +190,22 @@ class Material {
         this.energy = energy;
         this.dos = dos;
         this.materialId = materialId;
+    }
+
+    public void setPsf2(PolynomialSplineFunction psf2) {
+        this.psf2 = psf2;
+    }
+
+    public PolynomialSplineFunction getPsf2() {
+        return psf2;
+    }
+
+    public void setPsf(PolynomialSplineFunction psf) {
+        this.psf = psf;
+    }
+
+    public PolynomialSplineFunction getPsf() {
+        return psf;
     }
 
     public int getMaterialId() {
@@ -231,7 +261,8 @@ class Material {
         }
 
         stringBuilder.append("\n");
-        stringBuilder.append(dos.size() + " " + energy.size());
+        stringBuilder.append(dos.size() +
+                " " + energy.size());
 
         stringBuilder.append("\n");
 
