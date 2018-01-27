c002f607ed3941db868c8c27ba7fb557fb449d8e
Martin Pettersson <kontakt@martinpettersson.se>
2018-01-27 02:35:22 +0100

    We can now insert all material vectors in a set of tables.

--- BigVectorDistance.java
+++ BigVectorDistance.java
@@ -32,8 +32,9 @@ public class BigVectorDistance {
 
     private static ArrayList<Integer> mId;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.err.println("Hello World");
+
 
         try {
             mId = materialIndices(MATERIAL_IDS);
@@ -46,6 +47,9 @@ public class BigVectorDistance {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        //createDatabaseTables();
     }
 
     private static ArrayList<Integer> materialIndices(String fileName)
@@ -72,15 +76,49 @@ public class BigVectorDistance {
         return materialIds;
     }
 
+    private static void createDatabaseTables() throws SQLException {
+        java.sql.Connection connection
+                = DriverManager.getConnection
+                (VECTORS_URL,
+                        DATABASE_USER,
+                        DATABASE_PASSWORD);
+        int numberOfTables = 23;
+        for (int index = 0; index < numberOfTables; index++) {
+            String updateStatement = "CREATE TABLE dos_vectors_"
+                    + index + " (\n" +
+                    "    vector_id int,\n" +
+                    "    material_id int,\n" +
+                    "    cod_id int,\n" +
+                    "    parsed_dos_vector text,\n" +
+                    "    interpolated_dos_vector text,\n" +
+                    "    parsed_energy_vector text,\n" +
+                    "    close_materials text,\n" +
+                    "    hvb_e double\n" +
+                    ");";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(updateStatement);
+        }
+        connection.close();
+    }
+
     private static void
-    parseMaterials(ArrayList<Integer> indices) throws IOException {
+    parseMaterials(ArrayList<Integer> indices) throws IOException, SQLException {
         int numberOfCatches = 0;
+        int counter = 0;
+
+        java.sql.Connection connection
+                = DriverManager.getConnection
+                (VECTORS_URL,
+                        DATABASE_USER,
+                        DATABASE_PASSWORD);
+        int tableIndex = 0;
 
         for (int materialId : indices) {
             ArrayList<Double> dosVector = new ArrayList<>();
             ArrayList<Double> energyVector = new ArrayList<>();
             String extendedFilePath = FILE_PATH;
             extendedFilePath += materialId + "/DOS/dos.json";
+
             try {
                 BufferedReader br = new BufferedReader(
                         new FileReader(extendedFilePath));
@@ -118,10 +156,37 @@ public class BigVectorDistance {
                     new Material(energyVector, dosVector, materialId);
             materials.put(materialId, material);
             */
-            tmpPrintMaterial(dosVector, energyVector, materialId);
+            //tmpPrintMaterial(dosVector, energyVector, materialId);
+            String dosString = listToString(dosVector);
+            String energyString = listToString(energyVector);
+
+
+            String updateStatement = "INSERT INTO dos_vectors_" + tableIndex + " " +
+                    "(cod_id, parsed_dos_vector, parsed_energy_vector)\n" +
+                    "VALUES (" + materialId + ", '"+ dosString + "', '" + energyString + "')";
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(updateStatement);
+
+            counter++;
+            if (counter % 100 == 0)
+                System.err.println("Parsed " + counter);
+            if (counter % 1000 == 0) {
+                tableIndex++;
+                System.err.println("Updated table index to " + tableIndex);
+            }
+
         }
         System.err.println(
                 numberOfCatches + " could not be parsed.");
+        connection.close();
+    }
+
+    private static String listToString(ArrayList<Double> list) {
+        StringBuilder sb = new StringBuilder();
+        for (double value : list)
+            sb.append(value + " ");
+        return sb.toString();
     }
 
     private static void tmpPrintMaterial(ArrayList<Double> dos,
